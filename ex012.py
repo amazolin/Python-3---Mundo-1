8b763ebd205abccdd4164a7d1a92140c19cc9192
amazolin <danilozamai@hormail.com>
2025-08-12 00:54:19 -0300

    ex012

--- ex012.py
+++ ex012.py
@@ -1,4 +1,4 @@
 preco = float(input('Qual o preço do produto? '))
-desconto = preco * 0.05
+desconto = preco * 5/100
 precofinal = preco - desconto
 print('O valor do produto com desconto é R${:.2f}'.format(precofinal))
